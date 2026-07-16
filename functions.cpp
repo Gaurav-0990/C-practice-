a075fb5f4a9c3b04c4ead44fa534064596ffb4e1
Gaurav Negi <10217702722_cse@vips.edu>
2026-07-16 12:02:30 +0530

    Functions

---
+++ functions.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+//functions 
+//take 2 number and print its sum 
+
+// void printName(string name ){
+//     cout << "Hey " << name << endl ;
+//}
+
+
+
+// int sum (int a , int b ){
+//     int c = a + b ;
+//     return c ;
+//}
+
+void sum(int a , int b ){
+int c = a + b ;
+cout << c << endl ;
+}
+int main (){
+    // string name;
+    // cin >> name ;
+    // printName(name);
+
+    // string name2;
+    // cin >> name2;
+    // printName(name2);
+
+    // int a , b ;
+    // cin >>a >>b ;
+    // int res = sum (a,b);
+    // cout << res << endl ;
+
+    int a , b ;
+    cin >> a >> b ;
+    sum (a,b);
+
+    return 0;
+}
